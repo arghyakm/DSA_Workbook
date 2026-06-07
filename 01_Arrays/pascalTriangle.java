e6b549a81454cfde523da273c1f60ddaac282ae8
arghyakm <arghyakmondalofficial@gmail.com>
2026-06-07 11:44:00 +0530

    Leetcode 73: Set Matrix Zeros

---
+++ 01_Arrays/pascalTriangle.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class pascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (numRows == 0) {
+            return result;
+        }
+        
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        result.add(firstRow);
+        
+        if (numRows == 1) return result;
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prevRows = result.get(i - 1);
+            List<Integer> rows = new ArrayList<>();
+            
+            rows.add(1);
+
+            for (int j = 0; j < i - 1; j++) {
+                rows.add(prevRows.get(j) + prevRows.get(j + 1));
+            }
+            
+            rows.add(1);
+            result.add(rows);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the class
+        pascalTriangle pt = new pascalTriangle();
+        
+    
+        int numRows = 5;
+        
+        List<List<Integer>> result = pt.generate(numRows);
+        
+        
+        System.out.println("Pascal's Triangle for " + numRows + " rows:");
+        for (List<Integer> row : result) {
+            System.out.println(row);
+        }
+    }
+}
---
+++ 01_Arrays/setMatrixZeros.java
@@ -0,0 +1,37 @@
+class setMatrixZeros {
+    public void setZeroes(int[][] matrix) {
+        boolean fRow= false;
+        boolean fCol= false;
+
+        for(int i=0;i<matrix.length;i++){
+            for (int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    if(i==0) fRow= true;
+                    if(j==0) fCol= true;
+               
+                matrix[0][j]=0;
+                matrix[i][0]=0;
+                }
+            }
+        }
+
+         for(int i=1;i<matrix.length;i++){
+            for (int j=1;j<matrix[0].length;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                matrix[i][j]=0;
+                }
+            }
+        }
+        if(fRow){
+                    for(int j=0;j<matrix[0].length;j++ ){
+                        matrix[0][j]=0;
+                    }
+                }
+
+                if(fCol){
+                    for(int i=0;i<matrix.length;i++){
+                        matrix[i][0]=0;
+                    }
+                }
+    }
+}
